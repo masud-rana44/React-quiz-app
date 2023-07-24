c727af3e1fa021542362069e83e59710b74d95d9
masud-rana44 <mdmasudrana4488@gmail.com>
2023-07-25 02:14:50 +0600

    implemented start page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import StartScreen from "./components/StartScreen";
+import Error from "./components/Error";
+import Loader from "./components/Loader";
+import "./index.css";
+
+const initialState = {
+  loading: true,
+  error: false,
+  questions: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "ready":
+      return { ...state, loading: false, questions: action.payload };
+    case "error":
+      return { ...state, error: true };
+  }
+}
+
 function App() {
+  const [{ loading, error, questions }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numOfQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "ready", payload: data }))
+      .catch(() => dispatch({ type: "error" }));
+  }, []);
+
   return (
-    <div className="flex items-center justify-center bg-gray-800 min-h-screen">
-      App
+    <div className="app">
+      <Header />
+      <Main>
+        {error && <Error />}
+        {loading && <Loader />}
+        <StartScreen numOfQuestions={numOfQuestions} />
+      </Main>
     </div>
   );
 }
